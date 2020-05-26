f2d19e5f1a52d6a549bed0dc30576fee73adb54f
ciw-administrate <ciw@getadministrate.com>
2020-05-26 20:11:19 +0100

    add some very basic login assertions with data-cy identifiers

--- code/cypress/integration/loginapp.spec.js
+++ code/cypress/integration/loginapp.spec.js
@@ -1,5 +1,33 @@
 describe("UI TESTS", () => {
-  it("should navigate to google's website", () => {
-    cy.visit("www.google.com");
+  it("should load the login page correctly", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=login-text]").should("have.length", 1);
+    cy.get("[data-cy=login-text]").should("be.visible");
+  });
+
+  it("should not allow login when username is not provided", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=password]").type("password");
+    cy.get("[data-cy=submit-button]").click();
+    cy.get("[data-cy=login-text]").should("have.length", 1);
+    cy.get("[data-cy=homepage]").should("have.length", 0);
+  });
+
+  it("should not allow login when password is not provided", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=email]").type("email@email.com");
+    cy.get("[data-cy=submit-button]").click();
+    cy.get("[data-cy=login-text]").should("have.length", 1);
+    cy.get("[data-cy=homepage]").should("have.length", 0);
+  });
+
+  it("should login to the homepage with valid login credentials", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=email]").type("email@email.com");
+    cy.get("[data-cy=password]").type("password");
+    cy.get("[data-cy=submit-button]").click();
+    cy.get("[data-cy=logout-btn]").should("be.visible");
+    cy.get("[data-cy=logout-btn]").should("have.class", "btn-sm");
+    cy.get("[data-cy=logout-btn]").should("not.have.class", "lbl-fake");
   });
 });
