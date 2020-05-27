27f6b6c4728f1798ee530166fd89b814b5e73ddc
ciw-administrate <ciw@getadministrate.com>
2020-05-27 13:10:02 +0100

    test for fields having correct values

--- code/cypress/integration/loginapp.spec.js
+++ code/cypress/integration/loginapp.spec.js
@@ -24,6 +24,18 @@ describe("UI TESTS", () => {
   it("should login to the homepage with valid login credentials", () => {
     cy.visit("http://localhost:3000");
     cy.get("[data-cy=email]").type("email@email.com");
+    cy.get("[data-cy=email]").should("have.value", "email@email.com");
+    cy.get("[data-cy=password]").type("password");
+    cy.get("[data-cy=password]").should("have.value", "password");
+    cy.get("[data-cy=submit-button]").click();
+    cy.get("[data-cy=logout-btn]").should("be.visible");
+    cy.get("[data-cy=logout-btn]").should("have.class", "btn-sm");
+    cy.get("[data-cy=logout-btn]").should("not.have.class", "lbl-fake");
+  });
+
+  it("should contain correct input field values", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=email]").type("email@email.com");
     cy.get("[data-cy=password]").type("password");
     cy.get("[data-cy=submit-button]").click();
     cy.get("[data-cy=logout-btn]").should("be.visible");
