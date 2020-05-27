85c635ef61fcfcc4169ee49b9b50d856144dbe65
ciw-administrate <ciw@getadministrate.com>
2020-05-27 14:40:04 +0100

    nothing major

--- code/cypress/integration/loginapp.spec.js
+++ code/cypress/integration/loginapp.spec.js
@@ -5,8 +5,11 @@ describe("UI TESTS", () => {
     cy.get("[data-cy=login-text]").should("be.visible");
   });
 
-  it("should not allow login when username is not provided", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000");
+  });
+
+  it("should not allow login when username is not provided", () => {
     cy.get("[data-cy=password]").type("password");
     cy.get("[data-cy=submit-button]").click();
     cy.get("[data-cy=login-text]").should("have.length", 1);
@@ -14,7 +17,6 @@ describe("UI TESTS", () => {
   });
 
   it("should not allow login when password is not provided", () => {
-    cy.visit("http://localhost:3000");
     cy.get("[data-cy=email]").type("email@email.com");
     cy.get("[data-cy=submit-button]").click();
     cy.get("[data-cy=login-text]").should("have.length", 1);
@@ -22,7 +24,6 @@ describe("UI TESTS", () => {
   });
 
   it("should login to the homepage with valid login credentials", () => {
-    cy.visit("http://localhost:3000");
     cy.get("[data-cy=email]").type("email@email.com");
     cy.get("[data-cy=email]").should("have.value", "email@email.com");
     cy.get("[data-cy=password]").type("password");
@@ -34,12 +35,33 @@ describe("UI TESTS", () => {
   });
 
   it("should contain correct input field values", () => {
-    cy.visit("http://localhost:3000");
     cy.get("[data-cy=email]").type("email@email.com");
+    cy.get("[data-cy=email]").should("have.value", "email@email.com");
     cy.get("[data-cy=password]").type("password");
+    cy.get("[data-cy=password]").should("have.value", "password");
     cy.get("[data-cy=submit-button]").click();
     cy.get("[data-cy=logout-btn]").should("be.visible");
     cy.get("[data-cy=logout-btn]").should("have.class", "btn-sm");
     cy.get("[data-cy=logout-btn]").should("not.have.class", "lbl-fake");
+  });
+
+  it("should logout successfully", () => {
+    cy.get("[data-cy=email]").type("email@email.com");
+    cy.get("[data-cy=password]").type("password");
+    cy.get("[data-cy=submit-button]").click();
+    cy.get("[data-cy=logout-btn]").click();
+    cy.get("[data-cy=logout-text").should(
+      "contain.text",
+      "You are now logged out."
+    );
+    cy.get("[data-cy=logout-text").should(
+      "not.contain.text",
+      "You are now logged in."
+    );
+  });
+
+  it("should have existing elements", () => {
+    cy.get("[data-cy=login-text").should("exist");
+    cy.get("[data-cy=logout-text").should("not.exist");
   });
 });
